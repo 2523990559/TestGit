8dd4ed71e8406258adf9c64adefa458de595f33a
2523990559 <2523990559@qq.com>
2023-07-26 16:14:19 +0800

    新增tool为yaml.config使用，具体是路径

--- All_test_project/common/tool.py
+++ All_test_project/common/tool.py
@@ -6,9 +6,24 @@ def  get_now_time():
 
 #获取项目绝对路径
 def  get_project_path():
-    file_path=os.path.dirname(__file__)
-    print(file_path)
-    
+    project_name="All_test_project"
+    file_path=os.path.dirname(os.path.abspath(__file__))
+    #print(file_path)
+    return file_path[:file_path.find(project_name)+len(project_name)]
+
+
+def sep(path,add_sep_before=False,add_sep_after=False):
+    all_path=os.sep.join(path)
+    #print('具体的值',all_path)
+    if add_sep_after:
+        all_path=all_path+os.sep   #在地址前面加上分隔符合
+    if  add_sep_before:
+         all_path=os.sep+all_path #在地址后面加上分隔符合  
+    #print( '获取分隔符后面的地址',all_path)
+    return  all_path
+
+  
 if __name__=='__main__':
     #print(get_now_time())
     get_project_path()
+    sep()
